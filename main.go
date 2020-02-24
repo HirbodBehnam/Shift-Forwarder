b04d15cbb5ee25ba4446b7141c7e093f3a9205e3
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2020-02-24 21:05:32 +0330

    Added bitwise mode Changed command line arguments

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -11,32 +12,42 @@ import (
 var Verbose bool
 var Server bool
 var To string
+var BitwiseMode bool
 
-const VERSION = "1.0.0 / Build 1"
+const VERSION = "1.1.0 / Build 2"
 
 func main() {
-	if len(os.Args) == 1 || os.Args[1] == "-h" {
-		fmt.Println("Shift Forward Version", VERSION)
-		fmt.Println("Source https://github.com/HirbodBehnam/Shift-Forwarder")
-		fmt.Println("Use this app like this:")
-		fmt.Println("Server side:")
-		fmt.Println("./sf s <port> <to>")
-		fmt.Println("Example: ./sf s 1080 127.0.0.1:8888	This command accepts connections on port 1080 and forwards them to 127.0.0.1:8888")
-		fmt.Println()
-		fmt.Println("Client side:")
-		fmt.Println("./sf c <port> <to>")
-		fmt.Println("Example: ./sf c 8080 1.1.1.1:1080		This command accepts outgoing connections on port 8080 and forwards them to 1.1.1.1:1080")
-		os.Exit(0)
+	var port, interfaceAddress string
+	{ // Parse arguments
+		//showHelp := flag.Bool("help",false,"Show the help")
+		flag.BoolVar(&Server, "server", false, "Pass this argument to run as server application")
+		flag.BoolVar(&BitwiseMode, "bitwise", false, "Pass this argument to enable bitwise mode; Otherwise addition mode is used)")
+		flag.BoolVar(&Verbose, "verbose", false, "More logs")
+		showVersion := flag.Bool("version", false, "Show version")
+		flag.StringVar(&port, "port", "", "If this is server, the port that proxy listens on it; It this is client, the port that accepts the data")
+		flag.StringVar(&interfaceAddress, "interface", "", "Binding address. Server's default is 0.0.0.0 and client's is localhost")
+		flag.StringVar(&To, "to", "", "If this is server, the address that the data will be forwarded; If this is client, the server address")
+		flag.Parse()
+		if *showVersion {
+			fmt.Println("Shift Forward Version", VERSION)
+			fmt.Println("Source https://github.com/HirbodBehnam/Shift-Forwarder")
+			os.Exit(0)
+		}
+		if port == "" || To == "" {
+			fmt.Println("Please enter `port` and `to` values as argument. Pass --help to see help")
+			os.Exit(0)
+		}
+		if interfaceAddress == "" && !Server {
+			interfaceAddress = "localhost"
+		}
 	}
-	Server = os.Args[1] == "s"
-	fmt.Println("Server mode:", Server)
-	To = os.Args[3]
-	fmt.Println("To:", To)
-	Verbose = len(os.Args) > 4 && os.Args[4] == "-v"
-	fmt.Println("Verbose:", Verbose)
-
-	ln, err := net.Listen("tcp", ":"+os.Args[2])
-	fmt.Println("Listen on " + ":" + os.Args[2])
+	if Verbose {
+		fmt.Println("Verbose mode on")
+		fmt.Println("Server mode:", Server)
+		fmt.Println("Bitwise mode:", BitwiseMode)
+		fmt.Println("Listening on " + interfaceAddress + ":" + port)
+	}
+	ln, err := net.Listen("tcp", interfaceAddress+":"+port)
 	if err != nil {
 		panic(err)
 	}
@@ -57,7 +68,7 @@ func main() {
 func handleRequest(conn net.Conn) {
 	proxy, err := net.Dial("tcp", To)
 	if err != nil {
-		log.Println("Error:", err)
+		log.Println("Error on dialing:", err)
 		return
 	}
 
@@ -69,16 +80,23 @@ func copyIO(src, dest net.Conn) {
 	defer src.Close()
 	defer dest.Close()
 	var err error
-	if Server {
-		err = copyS(src, dest)
+	if BitwiseMode {
+		err = CopyBitwise(src, dest)
 	} else {
-		err = copyC(src, dest)
+		if Server {
+			err = ServerCopyAddition(src, dest)
+		} else {
+			err = ClientCopyAddition(src, dest)
+		}
 	}
 	if Verbose {
-		log.Println("Error on forward:", err)
+		if err != nil {
+			log.Println("Error on forward:", err)
+		}
 	}
 }
-func copyC(src, dst net.Conn) (err error) {
+
+func ClientCopyAddition(src, dst net.Conn) (err error) {
 	buf := make([]byte, 32*1024)
 	for {
 		nr, er := src.Read(buf)
@@ -105,13 +123,42 @@ func copyC(src, dst net.Conn) (err error) {
 	}
 	return err
 }
-func copyS(src, dst net.Conn) (err error) {
+
+func ServerCopyAddition(src, dst net.Conn) (err error) {
 	buf := make([]byte, 32*1024)
 	for {
 		nr, er := src.Read(buf)
 		if nr > 0 {
 			for i := 0; i < nr; i++ {
 				buf[i]--
+			}
+			nw, ew := dst.Write(buf[0:nr])
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return err
+}
+
+func CopyBitwise(src, dst net.Conn) (err error) {
+	buf := make([]byte, 32*1024)
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			for i := 0; i < nr; i++ {
+				buf[i] = ^buf[i]
 			}
 			nw, ew := dst.Write(buf[0:nr])
 			if ew != nil {
